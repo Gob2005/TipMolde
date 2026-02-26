2389cdd39d771fa02a384e94bee6205b6106a6f2
Gob2005 <8230153@estg.ipp.pt>
2026-02-26 12:30:47 +0000

    Adicionar arquivos de projeto.

---
+++ TipMolde/Program.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+
+string connectionString = "Server=localhost;Port=3306;Database=tipmolde;Uid=root;Pwd=password;";
+using (MySqlConnection conn = new MySqlConnection(connectionString))
+{
+    try
+    {
+        conn.Open();
+        Console.WriteLine("Conexão estabelecida com sucesso à TipMolde!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Erro: " + ex.Message);
+    }
+}
