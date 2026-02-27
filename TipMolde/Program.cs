f927e441b9f68008855a735838480cc0dd298c6f
Gob2005 <8230153@estg.ipp.pt>
2026-02-27 11:28:58 +0000

    correcoes finais

--- TipMolde/Program.cs
+++ TipMolde/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using TipMolde.Core.Interface;
+using TipMolde.Core.Interface.IUser;
+using TipMolde.Infrastutura.DB;
+using TipMolde.Infrastutura.Repositorio;
+using TipMolde.Infrastutura.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
